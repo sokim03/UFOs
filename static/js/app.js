772bebe6b4620674f074c29f04f7a64ae6228af1
sokim03 <sokim03@gmail.com>
2022-03-15 23:54:39 -0700

    updated files

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 //import the data from data.js
-//const tableData = data;
+const tableData = data;
 
 //Reference the HTML table using d3
-//var tbody = d3.select("tbody");
+var tbody = d3.select("tbody");
 
 //Simple JavaScript console.log statement
-//function printHello() {
-    ///console.log("Hello there!");
-//}
+function printHello() {
+    console.log("Hello there!");
+}
 
 //Creating table for UFO data
 function buildTable(data) {
@@ -29,3 +29,28 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+
+    //Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //Check to see if a date was entered and filter the
+    //data using that date.
+    if (date) {
+        //Apply 'filter' to the table data to only keep the
+        //rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebuild the table using the filtered data
+    //@Note: if no date was entered, then filteredData will just be
+    //the original tableData.
+    buildTable(filteredData);
+}
+
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
